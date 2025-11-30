464afbe5f47ed39bbe08e5382d3391886d15d01e
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-11-30 19:25:51 +0600

    Auth Route Added

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyJwt } from "../config/jwt";
+import { AppError } from "../utils/AppError";
+
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+export function requireAuth(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+    const token = req.cookies?.[process.env.COOKIE_NAME || "tb_access"] || req.header("Authorization")?.replace("Bearer ", "");
+    if (!token){ return next(AppError.unauthorized("No token provided"))
+    } 
+    const payload = verifyJwt(token);
+    req.user = payload;
+    return next();
+  } catch (err) {
+    return next(AppError.unauthorized("Invalid or expired token"));
+  }
+}
+
+export function requireRole(role: "ADMIN" | "USER") {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (!req.user){
+        return next(AppError.unauthorized("Authentication required! No user found..."));
+    }
+    if (req.user.role !== role){
+        
+        return next(AppError.forbidden("Forbidden"));
+    }
+    next();
+  };
+}
