d51934ef18ffeb9f80d3914cac3a81476b6d3d91
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-01 18:43:57 +0600

    TravelPlan Route Added

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from './modules/auth/auth.routes'
 import userRoutes from './modules/users/user.route'
 import { errorHandler } from "./middlewares/errorHandler";
-// import projectRoutes from './modules/project/project.routes'
+import travelPlanRoute from './modules/travelPlans/travelPlan.route'
 // import aboutRoutes from './modules/about/about.route'
 // import { errorHandler } from "./middleware/errorHandler";
 // import { seedAdmin } from "./utils/seedAdmin";
@@ -35,7 +35,7 @@ app.use('/api/auth', authRoutes);
 // app.use('/api/about', aboutRoutes);
 // app.use("/api/auth", authRoute);
 app.use("/api/users", userRoutes);
-// app.use("/api/travel-plans", travelPlanRoute);
+app.use("/api/travel-plans", travelPlanRoute);
 // app.use("/api/reviews", reviewRoute);
 // app.use("/api/payments", paymentRoute);
 
